c5242c06e97a2a4889a5ce1a06406dea14372ba9
andrew-suo <72462117+andrew-suo@users.noreply.github.com>
2026-06-13 19:23:19 +0800

    Add evaluation prompt optimizer

---
+++ mmap_optimizer/__init__.py
@@ -0,0 +1,5 @@
+"""MMAP optimizer package."""
+
+from mmap_optimizer.prompts import EvaluationRule, PromptIR, PromptVersion
+
+__all__ = ["EvaluationRule", "PromptIR", "PromptVersion"]
---
+++ mmap_optimizer/evaluation/__init__.py
@@ -0,0 +1,21 @@
+"""Evaluation prompt optimization APIs."""
+
+from mmap_optimizer.evaluation.prompt_optimizer import (
+    BehaviorSuiteResult,
+    EvalPromptBehaviorSuite,
+    EvaluationCase,
+    EvaluationPromptOptimizer,
+    OptimizationReport,
+    PatchDecision,
+    RuleBasedEvaluationHarness,
+)
+
+__all__ = [
+    "BehaviorSuiteResult",
+    "EvalPromptBehaviorSuite",
+    "EvaluationCase",
+    "EvaluationPromptOptimizer",
+    "OptimizationReport",
+    "PatchDecision",
+    "RuleBasedEvaluationHarness",
+]
